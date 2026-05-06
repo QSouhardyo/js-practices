1e1ea33743d29cbdcf8f78f3a22abc4f471da9a9
QSouhardyo <quaziaumio98@gmail.com>
2026-05-06 18:52:13 +0600

    unique

--- function.js
+++ function.js
@@ -7,18 +7,40 @@ function add(a, b) {
 
 add(5, 3)
 
-const numbers = [65, 48, 95, 60, 70, 75, 65]
+const numbers = [45, 48, 95, 60, 70, 75, 85]
 let sum = 0
+let odd = []
 function oddNumberAvrg() {
     for (num of numbers) {
         if (num % 2 === 1) {
+            odd.push(num);
             sum = num + sum
         }
 
     }
-    return sum;
+
+    let avrg = sum / odd.length
+
+
+    return avrg;
 }
 
 const result = oddNumberAvrg()
+console.log(odd);
 
 console.log(result);
+
+let students = ['jara', 'sara', 'raha', 'sara', 'tania', 'fatima', 'raha']
+function noDuplicate(array) {
+    let unique = []
+    for (const item of array) {
+        if (unique.includes(item) === false) {
+            unique.push(item)
+        }
+    }
+    return unique;
+}
+
+const newArray = noDuplicate(students);
+
+console.log(newArray);
